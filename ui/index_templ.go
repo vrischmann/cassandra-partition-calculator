d355b441eba3f678657e5976ba4a82b8626311af
Vincent Rischmann <vincent@rischmann.fr>
2024-04-07 12:56:46 +0200

    ui: update generated code

--- ui/index_templ.go
+++ ui/index_templ.go
@@ -43,7 +43,7 @@ func HeaderComponent(baseURL string, title string) templ.Component {
 		var templ_7745c5c3_Var3 string
 		templ_7745c5c3_Var3, templ_7745c5c3_Err = templ.JoinStringErrs(baseURL + "/assets/style.css")
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `ui/index.templ`, Line: 8, Col: 76}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `ui/index.templ`, Line: 8, Col: 77}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var3))
 		if templ_7745c5c3_Err != nil {
